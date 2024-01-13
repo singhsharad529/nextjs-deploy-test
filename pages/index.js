7c1dcb89bd40d2e71c87e50154337ee6c9ca6ac3
Sharad Kumar Singh <singhsharad529@gmail.com>
2024-01-13 16:13:23 +0530

    added new changes in home page

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1>Welcome to my app</h1>
+        <h1>Welcome back in my app</h1>
       </div>
     </>
   );
